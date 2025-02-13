b9ec2e54994e168bca1ea3b72a4704542cf3e543
Jideotetic <jideotetic@gmail.com>
2025-02-13 16:54:31 +0100

    Cleaning up

--- www/vite.config.js
+++ www/vite.config.js
@@ -31,7 +31,6 @@ export default defineConfig({
         },
         mimeTypes: {
             ".wasm": "application/wasm",
-            ".js": "application/js",
         },
         headers: {
             "Cross-Origin-Embedder-Policy": "require-corp",
